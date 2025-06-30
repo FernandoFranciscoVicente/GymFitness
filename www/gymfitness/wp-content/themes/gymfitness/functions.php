e232a8b5926e990010155036afa1268d4515abe5
Fernando VI. <f3rnandogaff@gmail.com>
2025-06-29 22:44:27 -0600

    Header y normalize

---
+++ www/gymfitness/wp-content/themes/gymfitness/functions.php
@@ -0,0 +1,31 @@
+<?php
+//Habilitar ciertas funciones desde wordpress
+//El nombre de la función suele empezar con el tema
+
+// --Registrando el menú dinámico--
+function gymfitness_menus(){
+    register_nav_menus( array(
+        //Arreglo asociativo (clave-valor)
+        //Aquí el gymfitness se refiere al Text Domain
+        'menu-principal' => __('Menu principal', 'gymfitness')
+    ) );
+}
+
+//Permite modificar la información
+//Los hooks crean eventos
+add_action('init', 'gymfitness_menus');
+
+
+// --Registrando la hoja de estilos
+function gymfitness_scripts_styles(){
+
+    //Añadimos normalize
+    wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), "8.0.1");
+    //Función de Wp que recibe la ruta de una hoja de estilos
+    //'style' es el identificador y get_stylesheet_uri() retorna la path de la hoja de estilos
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), "1.0.0");
+}
+
+add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
+?>
+
--- www/gymfitness/wp-content/themes/gymfitness/index.php
+++ www/gymfitness/wp-content/themes/gymfitness/index.php
@@ -1,5 +1,3 @@
-<h1>Hola mundo en WordPress</h1>
-
 <!--Coloca el lenguaje en que se instaló WordPress-->
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
@@ -7,10 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="widtsh=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php wp_head(); ?>
 </head>
 <body>
-    <header>
+    <header class="header">
+        <div class="contenedor barra-navegacion">
+            <div class="logo">
+                <!--Obtener la ruta de manera dinámica con PHP-->
+                <img src="
+                <?php echo get_template_directory_uri(); ?>
+                /img/logo.svg"
+                alt="imagen logotipo">
+            </div>
+        <!--Navegación dinámica-->
+        <?php
+        $args = array(
+            //Especificar el id del menú se requiere agregar 
+            'theme_location' => 'menu-principal',
+            //Cambiamos el div por un nav que contendrá el menú
+            'container' => 'nav',
+            //Especificamos la(s) clase(s) que tendrá el elemento nav
+            'container_class' => 'menu-principal'
+        );
+        //Nos permite mostrar un menú
+        wp_nav_menu($args);
+        ?>
         
+        </div>
     </header>
     <main>
 
